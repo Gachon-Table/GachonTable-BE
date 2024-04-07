5b66ba17f7825c73e4d7dfd2590b856e5e8402e2
노정완 <jeongwan.noh@gmail.com>
2024-04-07 15:39:17 +0900

    Initial commit

---
+++ src/main/java/site/gachontable/gachontablebe/GachonTableBeApplication.java
@@ -0,0 +1,13 @@
+package site.gachontable.gachontablebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachonTableBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachonTableBeApplication.class, args);
+	}
+
+}
