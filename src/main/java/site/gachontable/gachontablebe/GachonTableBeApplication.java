8b526a3b7449fe825eb9e9728112e3d516c49130
노정완 <jeongwan.noh@gmail.com>
2024-05-16 13:30:51 +0900

    [GTB-25] feat: BaseTimeEntity 설정

--- src/main/java/site/gachontable/gachontablebe/GachonTableBeApplication.java
+++ src/main/java/site/gachontable/gachontablebe/GachonTableBeApplication.java
@@ -2,7 +2,9 @@ package site.gachontable.gachontablebe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class GachonTableBeApplication {
 
