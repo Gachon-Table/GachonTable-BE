3bec0200771a8f54a892b3ed412d9731a0057fc9
노정완 <jeongwan.noh@gmail.com>
2024-05-16 15:38:52 +0900

    [GTB-25] feat: 주점 도메인 생성

---
+++ src/main/java/site/gachontable/gachontablebe/domain/pub/domain/Pub.java
@@ -0,0 +1,51 @@
+package site.gachontable.gachontablebe.domain.pub.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import site.gachontable.gachontablebe.domain.menu.domain.Menu;
+
+import java.util.List;
+
+@Entity(name = "pub")
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Pub {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer pubId;
+
+    @Column(nullable = false)
+    private String pubName;
+
+    @Column(nullable = false)
+    private String oneLiner;
+
+    @Column(columnDefinition = "char(13)", nullable = false)
+    private String pubTel;
+
+    @Column(nullable = false)
+    private Boolean studentCard;
+
+    @Column(nullable = false)
+    private String representativeMenu;
+
+    @Column(nullable = true)
+    private String pubLoc;
+
+    @Column(nullable = false)
+    private String pubThumbnail;
+
+    @OneToMany
+    @Column(nullable = false)
+    private List<Menu> menus;
+
+    @Column(nullable = false)
+    private Boolean openStatus;
+
+    @OneToMany
+    @Column(nullable = false)
+    private List<Waiting> waitingQueue;
+}
