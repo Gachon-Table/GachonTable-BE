ce7cbbdab00f0021810841441c71724902e8b99b
노정완 <jeongwan.noh@gmail.com>
2024-05-17 18:10:54 +0900

    [GTB-26] feat: 어드민 컨트롤러 생성

---
+++ src/main/java/site/gachontable/gachontablebe/domain/admin/presentation/AdminController.java
@@ -0,0 +1,39 @@
+package site.gachontable.gachontablebe.domain.admin.presentation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.gachontable.gachontablebe.domain.admin.dto.AdminLoginRequest;
+import site.gachontable.gachontablebe.domain.admin.usecase.AdminLogin;
+import site.gachontable.gachontablebe.global.error.ErrorResponse;
+import site.gachontable.gachontablebe.global.jwt.dto.JwtResponse;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController {
+    private final AdminLogin adminLogin;
+
+    @Operation(summary = "관리자 로그인", description = "관리자 계정으로 로그인합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody AdminLoginRequest request) {
+        JwtResponse tokens = adminLogin.execute(request.id(), request.password());
+        return ResponseEntity.ok(tokens);
+    }
+}
