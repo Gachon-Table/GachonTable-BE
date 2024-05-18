0efa5d0243a4133f8d3e32fadadee9e73ac7e906
노정완 <jeongwan.noh@gmail.com>
2024-05-18 10:22:23 +0900

    [GTB-26] fix: 어드민 repository UUID 적용

--- src/main/java/site/gachontable/gachontablebe/domain/admin/domain/repository/AdminRepository.java
+++ src/main/java/site/gachontable/gachontablebe/domain/admin/domain/repository/AdminRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import site.gachontable.gachontablebe.domain.admin.domain.Admin;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface AdminRepository extends JpaRepository<Admin, Long> {
+public interface AdminRepository extends JpaRepository<Admin, UUID> {
 
     Optional<Admin> findByAdminName(String adminName);
 }
