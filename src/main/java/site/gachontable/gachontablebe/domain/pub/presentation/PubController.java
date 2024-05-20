5b2a1d7f195050e4b1745eef2d3ed05fd74381b9
rootTiket <l20078829@gmail.com>
2024-05-20 15:54:54 +0900

    [GTB-29] feat: Api Response와 Operation 추가

--- src/main/java/site/gachontable/gachontablebe/domain/pub/presentation/PubController.java
+++ src/main/java/site/gachontable/gachontablebe/domain/pub/presentation/PubController.java
@@ -1,11 +1,17 @@
 package site.gachontable.gachontablebe.domain.pub.presentation;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.gachontable.gachontablebe.domain.pub.application.PubService;
 import site.gachontable.gachontablebe.domain.pub.dto.GetPubsResponseDto;
+import site.gachontable.gachontablebe.global.error.ErrorResponse;
 
 import java.util.List;
 
@@ -14,6 +20,14 @@ import java.util.List;
 public class PubController {
     private final PubService pubService;
 
+    @Operation(summary = "주점 목록", description = "전체 주점 목록을 가져옵니다")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200"),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @GetMapping("/pubs")
     public ResponseEntity<List<GetPubsResponseDto>> getPubs() {
         return ResponseEntity.ok(pubService.getPubs());
