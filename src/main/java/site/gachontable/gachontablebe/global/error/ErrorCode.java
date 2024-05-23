884c2efc6b27630a1ac3964146182a1b8a43b135
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-05-23 21:18:12 +0900

    [GTB-31] feat: 에러 코드 추가

--- src/main/java/site/gachontable/gachontablebe/global/error/ErrorCode.java
+++ src/main/java/site/gachontable/gachontablebe/global/error/ErrorCode.java
@@ -11,6 +11,8 @@ public enum ErrorCode {
     PASSWORD_NOT_MATCH(403, "PASSWORD_NOT_MATCH", "비밀번호가 일치하지 않습니다."),
     USER_NOT_FOUND(404, "USER_NOT_FOUND", "존재하지 않는 사용자입니다."),
     ADMIN_NOT_FOUND(404, "ADMIN_NOT_FOUND", "존재하지 않는 관리자입니다."),
+    WAITING_NOT_FOUND(404, "WAITING_NOT_FOUND", "존재하지 않는 대기열입니다."),
+    INVALID_QUEUEING_COUNT(400, "INVALID_QUEUEING_COUNT", "사용자의 대기열이 비어있습니다."),
     INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다.");
 
 
