6fc774ce9470725c2c43f662281ff6e2a81dc3dc
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-05-25 16:08:56 +0900

    [GTB-31] refactor: 오류 코드 재정렬 및 정리

--- src/main/java/site/gachontable/gachontablebe/global/error/ErrorCode.java
+++ src/main/java/site/gachontable/gachontablebe/global/error/ErrorCode.java
@@ -6,13 +6,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
+    EMPTY_QUEING_COUNT(400, "EMPTY_QUEING_COUNT", "사용자의 대기열이 비어있습니다."),
     INVALID_TOKEN(401, "INVALID_TOKEN", "유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN(403, "EXPIRED_TOKEN", "만료된 토큰입니다."),
     PASSWORD_NOT_MATCH(403, "PASSWORD_NOT_MATCH", "비밀번호가 일치하지 않습니다."),
     USER_NOT_FOUND(404, "USER_NOT_FOUND", "존재하지 않는 사용자입니다."),
     ADMIN_NOT_FOUND(404, "ADMIN_NOT_FOUND", "존재하지 않는 관리자입니다."),
     WAITING_NOT_FOUND(404, "WAITING_NOT_FOUND", "존재하지 않는 대기열입니다."),
-    INVALID_QUEUEING_COUNT(400, "INVALID_QUEUEING_COUNT", "사용자의 대기열이 비어있습니다."),
     INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 오류가 발생했습니다.");
 
 
