cfbab5b5a3e131f5fc43856f3d935e53c9a131e0
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-05-25 16:08:36 +0900

    [GTB-31] refactor: 불필요한 어노테이션 제거

--- src/main/java/site/gachontable/gachontablebe/domain/admin/dto/ForceCancelRequest.java
+++ src/main/java/site/gachontable/gachontablebe/domain/admin/dto/ForceCancelRequest.java
@@ -1,13 +1,8 @@
 package site.gachontable.gachontablebe.domain.admin.dto;
 
-import jakarta.persistence.Column;
-import lombok.Builder;
-
 import java.util.UUID;
 
-@Builder
 public record ForceCancelRequest(
-        @Column(columnDefinition = "BINARY(16)")
         UUID userId
 ) {
 }
--- src/main/java/site/gachontable/gachontablebe/domain/admin/usecase/ForceCancel.java
+++ src/main/java/site/gachontable/gachontablebe/domain/admin/usecase/ForceCancel.java
@@ -1,9 +1,7 @@
 package site.gachontable.gachontablebe.domain.admin.usecase;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import site.gachontable.gachontablebe.domain.user.Exception.UserNotFoundException;
 import site.gachontable.gachontablebe.domain.user.domain.User;
 import site.gachontable.gachontablebe.domain.user.domain.repository.UserRepository;
@@ -16,13 +14,11 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class ForceCancel {
 
     private final UserRepository userRepository;
     private final WaitingRepository waitingRepository;
 
-    @Transactional
     public String cancel(UUID Id) {
         User user = userRepository.findById(Id).orElseThrow(UserNotFoundException::new);
         Waiting waiting = waitingRepository.findByUser(user).orElseThrow(WaitingNotFoundException::new);
