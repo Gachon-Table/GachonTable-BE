59448bdda4b87906951fd1f81e7c9483cd6f1363
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-05-27 19:27:59 +0900

    [GTB-31] refactor: 예약 탐색 로직 수정

--- src/main/java/site/gachontable/gachontablebe/domain/waiting/domain/repository/WaitingRepository.java
+++ src/main/java/site/gachontable/gachontablebe/domain/waiting/domain/repository/WaitingRepository.java
@@ -1,11 +1,12 @@
 package site.gachontable.gachontablebe.domain.waiting.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import site.gachontable.gachontablebe.domain.pub.domain.Pub;
 import site.gachontable.gachontablebe.domain.user.domain.User;
 import site.gachontable.gachontablebe.domain.waiting.domain.Waiting;
 
 import java.util.Optional;
 
 public interface WaitingRepository extends JpaRepository<Waiting, Long> {
-    Optional<Waiting> findByUser(User user);
+    Optional<Waiting> findByUserAndPub(User user, Pub pub);
 }
