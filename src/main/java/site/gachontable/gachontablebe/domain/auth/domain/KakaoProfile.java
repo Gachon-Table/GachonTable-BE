7f5da8787731b2ca93648be352f37c720f804b91
노정완 <jeongwan.noh@gmail.com>
2024-06-01 16:03:52 +0900

    [GTB-27] fix: 중복되는 로직 변경

--- src/main/java/site/gachontable/gachontablebe/domain/auth/domain/KakaoProfile.java
+++ src/main/java/site/gachontable/gachontablebe/domain/auth/domain/KakaoProfile.java
@@ -1,18 +1,17 @@
 package site.gachontable.gachontablebe.domain.auth.domain;
 
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public record KakaoProfile(String email, String nickname) {
 
     public static KakaoProfile from(String jsonResponseBody) {
-        JsonElement element = JsonParser.parseString(jsonResponseBody);
+        JsonObject object = JsonParser.parseString(jsonResponseBody).getAsJsonObject();
 
-        JsonObject properties = element.getAsJsonObject().getAsJsonObject("properties");
+        JsonObject properties = object.getAsJsonObject("properties");
         String nickname = properties.get("nickname").getAsString();
 
-        JsonObject kakaoAccount = element.getAsJsonObject().getAsJsonObject("kakao_account");
+        JsonObject kakaoAccount = object.getAsJsonObject("kakao_account");
         String email = kakaoAccount.get("email").getAsString();
 
         return new KakaoProfile(email, nickname);
