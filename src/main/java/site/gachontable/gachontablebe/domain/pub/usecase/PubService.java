102fc1659591f67034b5c5085f3249acb0a20844
rootTiket <l20078829@gmail.com>
2024-06-01 23:35:07 +0900

    [GTB-32] feat: 주점 상세정보 가져오기 로직 구현

--- src/main/java/site/gachontable/gachontablebe/domain/pub/usecase/PubService.java
+++ src/main/java/site/gachontable/gachontablebe/domain/pub/usecase/PubService.java
@@ -2,8 +2,11 @@ package site.gachontable.gachontablebe.domain.pub.usecase;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import site.gachontable.gachontablebe.domain.menu.domain.Menu;
 import site.gachontable.gachontablebe.domain.pub.domain.Pub;
 import site.gachontable.gachontablebe.domain.pub.domain.repository.PubRepository;
+import site.gachontable.gachontablebe.domain.pub.exception.PubNotFoundException;
+import site.gachontable.gachontablebe.domain.pub.presentation.dto.GetPubsDetailResponse;
 import site.gachontable.gachontablebe.domain.pub.presentation.dto.GetPubsResponse;
 
 import java.util.List;
@@ -18,4 +21,9 @@ public class PubService {
 
         return pubList.stream().map(GetPubsResponse::of).toList();
 }
+    public GetPubsDetailResponse findPubDetail(Integer pubId) {
+        Pub pub = pubRepository.findByPubId(pubId).orElseThrow(PubNotFoundException::new);
+        List<Menu> menu = pub.getMenus();
+        return GetPubsDetailResponse.from(pub,menu);
+    }
 }
