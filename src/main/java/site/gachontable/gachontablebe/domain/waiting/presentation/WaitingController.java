2e38d43a443f06114335c330ad6869462ef67125
노정완 <jeongwan.noh@gmail.com>
2024-06-03 19:53:35 +0900

    [GTB-36] feat: 웨이팅 컨트롤러 생성

---
+++ src/main/java/site/gachontable/gachontablebe/domain/waiting/presentation/WaitingController.java
@@ -0,0 +1,39 @@
+package site.gachontable.gachontablebe.domain.waiting.presentation;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import site.gachontable.gachontablebe.domain.user.domain.User;
+import site.gachontable.gachontablebe.domain.waiting.presentation.dto.request.WaitingRequest;
+import site.gachontable.gachontablebe.domain.waiting.presentation.dto.response.WaitingResponse;
+import site.gachontable.gachontablebe.domain.waiting.usecase.CreateWaitingRemoteImpl;
+import site.gachontable.gachontablebe.global.error.ErrorResponse;
+
+@RestController
+@RequestMapping("/waiting")
+@RequiredArgsConstructor
+public class WaitingController {
+    private final CreateWaitingRemoteImpl createWaitingRemote;
+
+    @Operation(summary = "원격 웨이팅", description = "원격 웨이팅을 신규로 생성합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201"),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @PostMapping("/remote/{pubId}")
+    public ResponseEntity<WaitingResponse> createOnsite(@RequestHeader("Authorization") String authorizationHeader,
+                                                        @PathVariable Integer pubId,
+                                                        @RequestBody WaitingRequest request) {
+        User user = null;
+        createWaitingRemote.execute(user, pubId, request);
+        return ResponseEntity.ok(null);
+    }
+}
