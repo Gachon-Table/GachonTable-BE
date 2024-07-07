7b7b2abf4935eaf9b3ed379591d7289e918173a5
rootTiket <l20078829@gmail.com>
2024-07-08 00:21:41 +0900

    [GTB-43] feat: 웨이팅 내역을 가져오는 로직을 구현

---
+++ src/main/java/site/gachontable/gachontablebe/domain/waiting/usecase/GetWaitingHistory.java
@@ -0,0 +1,22 @@
+package site.gachontable.gachontablebe.domain.waiting.usecase;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import site.gachontable.gachontablebe.domain.user.domain.User;
+import site.gachontable.gachontablebe.domain.waiting.domain.Waiting;
+import site.gachontable.gachontablebe.domain.waiting.domain.repository.WaitingRepository;
+import site.gachontable.gachontablebe.domain.waiting.presentation.dto.response.WaitingHistoryResponse;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class GetWaitingHistory {
+    private final WaitingRepository waitingRepository;
+    public List<WaitingHistoryResponse> excute(User user) {
+        List<Waiting> waitings = waitingRepository.findAllByUser(user);
+        return waitings.stream()
+                .map(WaitingHistoryResponse::of)
+                .toList();
+    }
+}
