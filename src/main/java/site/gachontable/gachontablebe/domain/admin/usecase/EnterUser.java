f09ffda47fdc1db12ab93d28a025589773ae7115
노정완 <jeongwan.noh@gmail.com>
2024-07-08 23:55:12 +0900

    [GTB-46] fix: 인증 로직 수정

--- src/main/java/site/gachontable/gachontablebe/domain/admin/usecase/EnterUser.java
+++ src/main/java/site/gachontable/gachontablebe/domain/admin/usecase/EnterUser.java
@@ -3,7 +3,10 @@ package site.gachontable.gachontablebe.domain.admin.usecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import site.gachontable.gachontablebe.domain.admin.domain.Admin;
+import site.gachontable.gachontablebe.domain.admin.domain.repository.AdminRepository;
+import site.gachontable.gachontablebe.domain.admin.exception.AdminNotFoundException;
 import site.gachontable.gachontablebe.domain.admin.presentation.dto.request.EnterUserRequest;
+import site.gachontable.gachontablebe.domain.auth.domain.AuthDetails;
 import site.gachontable.gachontablebe.domain.pub.domain.Pub;
 import site.gachontable.gachontablebe.domain.pub.domain.repository.PubRepository;
 import site.gachontable.gachontablebe.domain.pub.exception.EmptyWaitingCountException;
@@ -19,10 +22,13 @@ public class EnterUser {
 
     private final PubRepository pubRepository;
     private final WaitingRepository waitingRepository;
+    private final AdminRepository adminRepository;
 
-    public String execute(Admin admin, EnterUserRequest request) {
-
-        Waiting waiting = waitingRepository.findById(request.waitingId()).orElseThrow(WaitingNotFoundException::new);
+    public String execute(AuthDetails authDetails, EnterUserRequest request) {
+        Admin admin = adminRepository.findById(authDetails.getUuid()).
+                orElseThrow(AdminNotFoundException::new);
+        Waiting waiting = waitingRepository.findById(request.waitingId()).
+                orElseThrow(WaitingNotFoundException::new);
         Pub pub = waiting.getPub();
 
         if (!pub.equals(admin.getPub())) {
