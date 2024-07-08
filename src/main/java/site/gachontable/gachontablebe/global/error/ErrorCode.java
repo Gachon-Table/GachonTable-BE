15e6c1a06c35e33720e08ffe7d6bea0b235bd2ce
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-07-08 20:51:30 +0900

    [GTB-42] feat: 에러 코드 추가

--- src/main/java/site/gachontable/gachontablebe/global/error/ErrorCode.java
+++ src/main/java/site/gachontable/gachontablebe/global/error/ErrorCode.java
@@ -7,9 +7,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     EMPTY_QUEUEING_COUNT(400, "EMPTY_QUEUEING_COUNT", "사용자의 대기열이 비어있습니다."),
+    EMPTY_WAITING_COUNT(400, "EMPTY_WAITING_COUNT", "주점에 대기열이 없습니다."),
     INVALID_TOKEN(401, "INVALID_TOKEN", "유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN(403, "EXPIRED_TOKEN", "만료된 토큰입니다."),
     PASSWORD_NOT_MATCH(403, "PASSWORD_NOT_MATCH", "비밀번호가 일치하지 않습니다."),
+    PUB_MISMATCH(403, "PUB_MISMATCH", "요청하는 주점이 일치하지 않습니다."),
     USER_NOT_FOUND(404, "USER_NOT_FOUND", "존재하지 않는 사용자입니다."),
     ADMIN_NOT_FOUND(404, "ADMIN_NOT_FOUND", "존재하지 않는 관리자입니다."),
     WAITING_NOT_FOUND(404, "WAITING_NOT_FOUND", "존재하지 않는 대기열입니다."),
