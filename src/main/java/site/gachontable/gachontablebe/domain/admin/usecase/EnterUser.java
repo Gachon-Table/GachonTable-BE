67e7df4a6c601e68da79fbb10b4b00d52d98b831
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-07-08 22:17:56 +0900

    [GTB-42] refactor: EnterUser.execute 메서드 인자 변경

--- src/main/java/site/gachontable/gachontablebe/domain/admin/usecase/EnterUser.java
+++ src/main/java/site/gachontable/gachontablebe/domain/admin/usecase/EnterUser.java
@@ -3,6 +3,7 @@ package site.gachontable.gachontablebe.domain.admin.usecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import site.gachontable.gachontablebe.domain.admin.domain.Admin;
+import site.gachontable.gachontablebe.domain.admin.presentation.dto.request.EnterUserRequest;
 import site.gachontable.gachontablebe.domain.pub.domain.Pub;
 import site.gachontable.gachontablebe.domain.pub.domain.repository.PubRepository;
 import site.gachontable.gachontablebe.domain.pub.exception.EmptyWaitingCountException;
@@ -19,9 +20,9 @@ public class EnterUser {
     private final PubRepository pubRepository;
     private final WaitingRepository waitingRepository;
 
-    public String execute(Admin admin, Long waitingId) {
+    public String execute(Admin admin, EnterUserRequest request) {
 
-        Waiting waiting = waitingRepository.findById(waitingId).orElseThrow(WaitingNotFoundException::new);
+        Waiting waiting = waitingRepository.findById(request.waitingId()).orElseThrow(WaitingNotFoundException::new);
         Pub pub = waiting.getPub();
 
         if (!pub.equals(admin.getPub())) {
