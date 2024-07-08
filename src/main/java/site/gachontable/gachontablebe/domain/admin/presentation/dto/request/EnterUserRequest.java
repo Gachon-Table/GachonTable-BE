0c4849475998b40f163f3478ed3dca8224a3c11e
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-07-08 20:48:37 +0900

    [GTB-42] refactor: 로직 변경으로 인한 DTO 수정

--- src/main/java/site/gachontable/gachontablebe/domain/admin/presentation/dto/request/EnterUserRequest.java
+++ src/main/java/site/gachontable/gachontablebe/domain/admin/presentation/dto/request/EnterUserRequest.java
@@ -1,8 +1,6 @@
 package site.gachontable.gachontablebe.domain.admin.presentation.dto.request;
 
-import java.util.UUID;
-
 public record EnterUserRequest(
-        UUID userId
+        Long waitingId
 ) {
 }
