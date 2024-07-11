85792b2932e2143a4df275d96e140b6507738e54
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-07-11 10:57:19 +0900

    [GTB-53] feat: 부재중인 사용자 예약 강제 취소 로직 구현

--- src/main/java/site/gachontable/gachontablebe/domain/admin/usecase/CallUser.java
+++ src/main/java/site/gachontable/gachontablebe/domain/admin/usecase/CallUser.java
@@ -8,17 +8,25 @@ import site.gachontable.gachontablebe.domain.admin.exception.AdminNotFoundExcept
 import site.gachontable.gachontablebe.domain.admin.presentation.dto.request.CallUserRequest;
 import site.gachontable.gachontablebe.domain.auth.domain.AuthDetails;
 import site.gachontable.gachontablebe.domain.pub.domain.Pub;
+import site.gachontable.gachontablebe.domain.pub.domain.repository.PubRepository;
 import site.gachontable.gachontablebe.domain.pub.exception.PubMismatchException;
 import site.gachontable.gachontablebe.domain.waiting.domain.Waiting;
 import site.gachontable.gachontablebe.domain.waiting.domain.repository.WaitingRepository;
 import site.gachontable.gachontablebe.domain.waiting.exception.WaitingNotFoundException;
+import site.gachontable.gachontablebe.domain.waiting.type.Status;
 import site.gachontable.gachontablebe.global.success.SuccessCode;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class CallUser {
     private final WaitingRepository waitingRepository;
     private final AdminRepository adminRepository;
+    private final PubRepository pubRepository;
+    private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
 
     public String execute(AuthDetails authDetails, CallUserRequest request) {
         Admin admin = adminRepository.findById(authDetails.getUuid()).
@@ -34,11 +42,36 @@ public class CallUser {
         updateWaitingStatusToAvailable(waiting);
         // TODO : 카카오 알림톡 전송
 
+        // 사용자가 5분 안에 응답하는지 확인하기 위해 작업을 예약합니다.
+        executorService.schedule(() -> {
+            if (getUpdatedhWaitingStatus(request).equals(Status.AVAILABLE)) {
+                // 사용자가 5분 이내에 응답하지 않으면 예약을 취소합니다.
+                updateWaitingStatusToCanceled(waiting);
+                decreaseWaitingCount(pub);
+                // TODO : 카카오 알림톡 전송
+            }
+        }, 5, TimeUnit.MINUTES);
+
         return SuccessCode.USER_CALL_SUCCESS.getMessage();
     }
 
     private void updateWaitingStatusToAvailable(Waiting waiting) {
         waiting.available();
         waitingRepository.save(waiting);
+    }
+
+    private void updateWaitingStatusToCanceled(Waiting waiting) {
+        waiting.cancel();
+        waitingRepository.save(waiting);
+    }
+
+    private void decreaseWaitingCount(Pub Pub) {
+        Pub.decreaseWaitingCount();
+        pubRepository.save(Pub);
+    }
+
+    private Status getUpdatedhWaitingStatus(CallUserRequest request) {
+        return waitingRepository.findById(request.waitingId()).
+                orElseThrow(WaitingNotFoundException::new).getWaitingStatus();
     }
 }
--- src/main/java/site/gachontable/gachontablebe/global/success/SuccessCode.java
+++ src/main/java/site/gachontable/gachontablebe/global/success/SuccessCode.java
@@ -10,7 +10,8 @@ public enum SuccessCode {
     REMOTE_WAITING_SUCCESS(201, "REMOTE_WAITING_SUCCESS", "원격 웨이팅에 성공하였습니다."),
     ONSITE_WAITING_SUCCESS(201, "ONSITE_WAITING_SUCCESS", "현장 웨이팅에 성공하였습니다."),
     USER_CALL_SUCCESS(201, "USER_CALL_SUCCESS", "사용자 호출에 성공하였습니다."),
-    WAITING_CANCEL_SUCCESS(201, "WAITING_CANCEL_SUCCESS", "웨이팅 취소에 성공하였습니다.");
+    WAITING_CANCEL_SUCCESS(201, "WAITING_CANCEL_SUCCESS", "웨이팅 취소에 성공하였습니다."),
+    WAITING_FORCE_CANCEL_SUCCESS(201, "WAITING_FORCE_CANCEL_SUCCESS", "웨이팅 강제 취소에 성공하였습니다.");
 
     private final int httpStatus;
     private final String code;
