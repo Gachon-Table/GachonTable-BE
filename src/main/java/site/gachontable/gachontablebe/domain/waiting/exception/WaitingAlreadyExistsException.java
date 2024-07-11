7a665afb83bed3517d9e81f5697452eb96d42380
rootTiket <l20078829@gmail.com>
2024-07-11 17:36:39 +0900

    [GTB-49] refactor: 예외 처리 명명 변경

--- src/main/java/site/gachontable/gachontablebe/domain/waiting/exception/WaitingAlreadyExistsException.java
+++ src/main/java/site/gachontable/gachontablebe/domain/waiting/exception/WaitingAlreadyExistsException.java
@@ -3,8 +3,8 @@ package site.gachontable.gachontablebe.domain.waiting.exception;
 import site.gachontable.gachontablebe.global.error.ErrorCode;
 import site.gachontable.gachontablebe.global.error.exception.ServiceException;
 
-public class WaitingAlreadyExistException extends ServiceException {
-    public WaitingAlreadyExistException() {
+public class WaitingAlreadyExistsException extends ServiceException {
+    public WaitingAlreadyExistsException() {
         super(ErrorCode.WAITING_ALREADY_EXIST);
     }
 }
