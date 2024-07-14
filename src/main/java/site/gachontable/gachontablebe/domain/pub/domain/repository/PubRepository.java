b8e0c957afd68459591692bbfdca7642e3300ec2
노정완 <jeongwan.noh@gmail.com>
2024-07-14 14:36:09 +0900

    feat: 주점 검색 쿼리 작성

--- src/main/java/site/gachontable/gachontablebe/domain/pub/domain/repository/PubRepository.java
+++ src/main/java/site/gachontable/gachontablebe/domain/pub/domain/repository/PubRepository.java
@@ -1,6 +1,8 @@
 package site.gachontable.gachontablebe.domain.pub.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import site.gachontable.gachontablebe.domain.pub.domain.Pub;
 
 import java.util.List;
@@ -8,4 +10,7 @@ import java.util.Optional;
 
 public interface PubRepository extends JpaRepository<Pub, Integer> {
     Optional<Pub> findByPubId(Integer pubId);
+
+    @Query("SELECT DISTINCT p FROM pub p WHERE p.pubName LIKE %:keyword% OR p.oneLiner LIKE %:keyword%")
+    List<Pub> findAllByTitleOrTextDataContaining(@Param("keyword") String keyword);
 }
