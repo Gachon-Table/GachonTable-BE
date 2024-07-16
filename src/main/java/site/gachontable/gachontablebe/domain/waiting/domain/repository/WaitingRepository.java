f0f63362d0d8e645b40cd62d16f8a1926f60c530
rootTiket <l20078829@gmail.com>
2024-07-16 12:55:18 +0900

    HOTFIX: 원격 웨이팅이 두번이상 되지 않는 오류 수정

--- src/main/java/site/gachontable/gachontablebe/domain/waiting/domain/repository/WaitingRepository.java
+++ src/main/java/site/gachontable/gachontablebe/domain/waiting/domain/repository/WaitingRepository.java
@@ -8,11 +8,13 @@ import site.gachontable.gachontablebe.domain.waiting.type.Status;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.OptionalInt;
 import java.util.UUID;
 
 public interface WaitingRepository extends JpaRepository<Waiting, UUID> {
     List<Waiting> findAllByTel(String tel);
     Optional<Waiting> findByTel(String tel);
+    Optional<Waiting> findByTelAndPub(String tel, Pub pub);
 
     List<Waiting> findAllByPubAndWaitingStatusOrWaitingStatusOrderByCreatedAtAsc(Pub pub, Status waiting, Status available);
     List<Waiting> findAllByUserAndWaitingStatusOrWaitingStatus(User user, Status entered, Status canceled);
