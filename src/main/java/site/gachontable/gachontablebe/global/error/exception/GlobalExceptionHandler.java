b958829ccbe4389c1073d7e7a9458fca13e85d2d
노정완 <jeongwan.noh@gmail.com>
2024-07-19 23:26:26 +0900

    [GTB-63] fix: 전역예외처리 핸들링 추가

--- src/main/java/site/gachontable/gachontablebe/global/error/exception/GlobalExceptionHandler.java
+++ src/main/java/site/gachontable/gachontablebe/global/error/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package site.gachontable.gachontablebe.global.error.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import site.gachontable.gachontablebe.global.error.ErrorCode;
 import site.gachontable.gachontablebe.global.error.ErrorResponse;
 
 @RestControllerAdvice
@@ -10,5 +13,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ServiceException.class)
     public ErrorResponse handleException(ServiceException e){
         return new ErrorResponse(e.getErrorCode());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        e.printStackTrace();
+        ErrorResponse response = new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
