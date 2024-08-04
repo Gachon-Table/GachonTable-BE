9e53f8c7085be370481e1bab0fbe92cb8dab5044
노정완 <jeongwan.noh@gmail.com>
2024-08-04 23:12:36 +0900

    [GTB-61] feat: 락 어노테이션 선언 시 수행되는 AOP 클래스 생성

---
+++ src/main/java/site/gachontable/gachontablebe/global/config/aop/RedissonLockAop.java
@@ -0,0 +1,57 @@
+package site.gachontable.gachontablebe.global.config.aop;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+import site.gachontable.gachontablebe.global.config.parser.CustomSpringELParser;
+import site.gachontable.gachontablebe.global.config.redis.RedissonLock;
+
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class RedissonLockAop {
+    private static final String REDISSON_LOCK_PREFIX = "LOCK:";
+
+    private final RedissonClient redissonClient;
+    private final AopForTransaction aopForTransaction;
+
+    @Around("@annotation(site.gachontable.gachontablebe.global.config.redis.RedissonLock)")
+    public Object lock(final ProceedingJoinPoint joinPoint) throws Throwable {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        RedissonLock redissonLock = method.getAnnotation(RedissonLock.class);
+
+        String key = REDISSON_LOCK_PREFIX + CustomSpringELParser
+                .getDynamicValue(signature.getParameterNames(), joinPoint.getArgs(), redissonLock.key());
+        RLock rLock = redissonClient.getLock(key);
+
+        try {
+            boolean available = rLock.tryLock(redissonLock.waitTime(), redissonLock.leaseTime(), redissonLock.timeUnit());
+            if (!available) {
+                log.info("Lock 획득 실패 : {}", key);
+                return false;
+            }
+
+            log.info("Lock 수행 : {}", key);
+            return aopForTransaction.proceed(joinPoint);
+        } catch (InterruptedException e) {
+            throw new InterruptedException();
+        } finally {
+            try {
+                log.info("Lock 해제 : {}", key);
+                rLock.unlock();
+            } catch (IllegalMonitorStateException e) {
+                log.info("이미 해제된 Lock : {} {}", method.getName(), key);
+            }
+        }
+    }
+}
