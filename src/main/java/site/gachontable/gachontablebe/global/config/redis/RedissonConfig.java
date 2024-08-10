206042f3e72b961086e5152a80a56841b409dd43
노정완 <jeongwan.noh@gmail.com>
2024-08-11 02:19:23 +0900

    HOTFIX: Redisson 암호 설정

--- src/main/java/site/gachontable/gachontablebe/global/config/redis/RedissonConfig.java
+++ src/main/java/site/gachontable/gachontablebe/global/config/redis/RedissonConfig.java
@@ -18,13 +18,17 @@ public class RedissonConfig {
     @Value("${spring.data.redis.database}")
     private int redisDatabase;
 
+    @Value("${spring.data.redis.password}")
+    private String redisPassword;
+
     private static final String REDISSON_HOST_PREFIX = "redis://";
 
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
         config.useSingleServer()
-                .setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort + "/" + redisDatabase);
+                .setAddress(REDISSON_HOST_PREFIX + redisHost + ":" + redisPort + "/" + redisDatabase)
+                .setPassword(redisPassword);
         return Redisson.create(config);
     }
 }
