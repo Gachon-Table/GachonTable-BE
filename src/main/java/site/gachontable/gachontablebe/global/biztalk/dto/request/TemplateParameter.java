23c827828f5e3a9ca63ea9de37f5b12dffb53acd
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-08-29 03:13:40 +0900

    [GTB-69] refactor: 알림톡 전송 로직 변경

--- src/main/java/site/gachontable/gachontablebe/global/biztalk/dto/request/TemplateParameter.java
+++
@@ -1,4 +0,0 @@
-package site.gachontable.gachontablebe.global.biztalk.dto.request;
-
-public interface TemplateParameter {
-}
--- src/main/java/site/gachontable/gachontablebe/global/biztalk/sendBiztalk.java
+++ src/main/java/site/gachontable/gachontablebe/global/biztalk/sendBiztalk.java
@@ -1,82 +1,55 @@
 package site.gachontable.gachontablebe.global.biztalk;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
+import net.nurigo.sdk.NurigoApp;
+import net.nurigo.sdk.message.model.KakaoOption;
+import net.nurigo.sdk.message.model.Message;
+import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
+import net.nurigo.sdk.message.response.SingleMessageSentResponse;
+import net.nurigo.sdk.message.service.DefaultMessageService;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-import site.gachontable.gachontablebe.global.biztalk.dto.request.TemplateParameter;
-import site.gachontable.gachontablebe.global.biztalk.dto.response.BiztalkResponse;
+import org.springframework.context.annotation.Configuration;
 import site.gachontable.gachontablebe.global.biztalk.exception.BiztalkSendFailException;
-import site.gachontable.gachontablebe.global.biztalk.exception.InvalidKeyException;
-import site.gachontable.gachontablebe.global.biztalk.dto.request.BiztalkRequest;
-import site.gachontable.gachontablebe.global.biztalk.dto.request.BiztalkRequest.Recipient;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Objects;
 
 
-@Service
-@Slf4j
+@Configuration
 public class sendBiztalk {
 
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final DefaultMessageService defaultMessageService;
+    private final String pfId;
+    private final String senderPhoneNumber;
 
-    @Value("${biztalk.app_key}")
-    String appkey;
-
-    @Value("${biztalk.secret_key}")
-    String secretKey;
-
-    @Value("${biztalk.sender_key}")
-    String senderKey;
-
-    public void execute(String templateCode, String phoneNumber, TemplateParameter templateParameter) {
-
-        Recipient recipient = new Recipient(phoneNumber, templateParameter);
-
-        BiztalkRequest biztalkRequest = new BiztalkRequest(senderKey, templateCode, List.of(recipient));
-
-        try {
-            BiztalkResponse biztalkResponse;
-            try (InputStream inputStream = getConnection(biztalkRequest).getInputStream()) {
-                biztalkResponse = objectMapper.readValue(inputStream, BiztalkResponse.class);
-            }
-
-            if (!biztalkResponse.header().isSuccessful()){
-                log.error("errorCode: {}, errorMsg: {}", biztalkResponse.header().resultCode(), biztalkResponse.header().resultMessage());
-                throw new BiztalkSendFailException();
-            }
-
-        } catch (IOException e) {
-            throw new BiztalkSendFailException();
-        }
+    public sendBiztalk(
+            @Value("${biztalk.app_key}") String appkey,
+            @Value("${biztalk.secret_key}") String secretKey,
+            @Value("${biztalk.pfId}") String pfId,
+            @Value("${biztalk.sender_phoneNumber}") String senderPhoneNumber
+    ) {
+        this.defaultMessageService = NurigoApp.INSTANCE.initialize(appkey, secretKey, "https://api.solapi.com");
+        this.pfId = pfId;
+        this.senderPhoneNumber = senderPhoneNumber;
     }
 
-    private HttpURLConnection getConnection(BiztalkRequest biztalkRequest) throws IOException {
-        URL url = new URL("https://api-alimtalk.cloud.toast.com/alimtalk/v2.3/appkeys/" + appkey + "/messages");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("X-Secret-Key", secretKey);
-        connection.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
-        connection.setDoOutput(true);
+    public void execute(String templateId, String phoneNumber, HashMap<String, String> variables) {
+        KakaoOption kakaoOption = new KakaoOption();
+        kakaoOption.setDisableSms(true);
 
-        String jsonRequest = objectMapper.writeValueAsString(biztalkRequest);
+        kakaoOption.setPfId(pfId);
+        kakaoOption.setTemplateId(templateId);
+        kakaoOption.setVariables(variables);
 
-        try (OutputStream os = connection.getOutputStream()) {
-            byte[] input = jsonRequest.getBytes(StandardCharsets.UTF_8);
-            os.write(input, 0, input.length);
+        Message message = new Message();
+        message.setFrom(senderPhoneNumber);
+        message.setTo(phoneNumber);
+        message.setKakaoOptions(kakaoOption);
+
+        SingleMessageSentResponse response = defaultMessageService.sendOne(new SingleMessageSendingRequest(message));
+
+        if (!Objects.equals(response.getStatusCode(), String.valueOf(HttpURLConnection.HTTP_OK))) {
+            throw new BiztalkSendFailException();
         }
-
-        int responseCode = connection.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_UNAUTHORIZED) {
-            throw new InvalidKeyException();
-        }
-
-        return connection;
     }
 }
