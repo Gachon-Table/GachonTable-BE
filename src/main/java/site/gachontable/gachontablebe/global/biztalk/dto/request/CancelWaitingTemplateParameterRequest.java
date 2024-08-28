8cc9f21626ac105279f0251e79436b3e92ba75a4
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-08-29 03:13:40 +0900

    [GTB-69] refactor: 알림톡 전송 로직 변경

--- src/main/java/site/gachontable/gachontablebe/global/biztalk/dto/request/CancelWaitingTemplateParameterRequest.java
+++
@@ -1,9 +0,0 @@
-package site.gachontable.gachontablebe.global.biztalk.dto.request;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record CancelWaitingTemplateParameterRequest(
-        @JsonProperty("pubName")
-        String pubName
-) implements TemplateParameter {
-}
