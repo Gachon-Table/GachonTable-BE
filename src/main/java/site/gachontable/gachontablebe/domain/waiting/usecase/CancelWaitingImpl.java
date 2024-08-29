c937119d1ab03c8aadfd51fe478ba3a2b40ef358
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2024-08-30 01:04:44 +0900

    [GTB-69] refactor: 웨이팅 취소 카카오 알림톡 로직 변경

--- src/main/java/site/gachontable/gachontablebe/domain/waiting/usecase/CancelWaitingImpl.java
+++ src/main/java/site/gachontable/gachontablebe/domain/waiting/usecase/CancelWaitingImpl.java
@@ -1,6 +1,8 @@
 package site.gachontable.gachontablebe.domain.waiting.usecase;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import site.gachontable.gachontablebe.domain.pub.domain.Pub;
 import site.gachontable.gachontablebe.domain.waiting.domain.Waiting;
@@ -10,17 +12,19 @@ import site.gachontable.gachontablebe.domain.waiting.presentation.dto.request.Ca
 import site.gachontable.gachontablebe.domain.waiting.presentation.dto.response.WaitingResponse;
 import site.gachontable.gachontablebe.global.config.redis.RedissonLock;
 import site.gachontable.gachontablebe.global.biztalk.sendBiztalk;
-import site.gachontable.gachontablebe.global.biztalk.dto.request.TemplateParameter;
-import site.gachontable.gachontablebe.global.biztalk.dto.request.CancelWaitingTemplateParameterRequest;
 import site.gachontable.gachontablebe.global.success.SuccessCode;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class CancelWaitingImpl implements CancelWaiting {
-    private static final String TEMPLATE_CODE = "CANCEL";
-  
     private final WaitingRepository waitingRepository;
     private final sendBiztalk sendBiztalk;
+
+    @Value("${biztalk.templateId.cancel}")
+    private String TEMPLATE_CODE;
 
     @RedissonLock(key = "#lockKey")
     @Override
@@ -30,12 +34,11 @@ public class CancelWaitingImpl implements CancelWaiting {
 
         waiting.cancel();
         waiting.getPub().decreaseWaitingCount();
-      
+
         // TODO : 카카오 알림톡 전송
         Pub pub = waiting.getPub();
-        TemplateParameter templateParameter = new CancelWaitingTemplateParameterRequest(pub.getPubName());
-        sendBiztalk.execute(TEMPLATE_CODE, waiting.getTel(), templateParameter);
-      
+        sendBiztalk.execute(TEMPLATE_CODE, waiting.getTel(), (HashMap<String, String>) Map.of("#{pub}", pub.getPubName()));
+
         return new WaitingResponse(true, SuccessCode.WAITING_CANCEL_SUCCESS.getMessage());
     }
 }
