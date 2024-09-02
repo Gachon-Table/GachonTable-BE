ae5b460f91966f8144ec0530ab7a902284073e08
노정완 <jeongwan.noh@gmail.com>
2024-09-02 22:24:54 +0900

    [GTB-68] fix: 시간 포맷팅 적용

--- src/main/java/site/gachontable/gachontablebe/domain/admin/presentation/dto/response/SeatingsResponse.java
+++ src/main/java/site/gachontable/gachontablebe/domain/admin/presentation/dto/response/SeatingsResponse.java
@@ -4,17 +4,20 @@ import lombok.Builder;
 import site.gachontable.gachontablebe.domain.seating.domain.Seating;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.UUID;
 
 public record SeatingsResponse(List<SeatingResponse> seatings) {
 
     @Builder
-    public record SeatingResponse(Integer tableNum, LocalDateTime exitTime, UUID waitingId) {
+    public record SeatingResponse(Integer tableNum, String exitTime, UUID waitingId) {
         public static SeatingResponse from(Seating seating) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+
             return SeatingResponse.builder()
                     .tableNum(seating.getSeatingNum())
-                    .exitTime(seating.getExitTime())
+                    .exitTime(seating.getExitTime().format(formatter))
                     .waitingId(seating.getWaiting().getWaitingId())
                     .build();
         }
