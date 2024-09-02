563e6eba72a9752ae0887c3d2660327efffb4555
노정완 <jeongwan.noh@gmail.com>
2024-09-02 19:27:33 +0900

    [GTB-68] feat: 주점 테이블 조회 반환 DTO 생성

---
+++ src/main/java/site/gachontable/gachontablebe/domain/admin/presentation/dto/response/SeatingsResponse.java
@@ -0,0 +1,22 @@
+package site.gachontable.gachontablebe.domain.admin.presentation.dto.response;
+
+import lombok.Builder;
+import site.gachontable.gachontablebe.domain.seating.domain.Seating;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public record SeatingsResponse(List<SeatingResponse> seatings) {
+
+    @Builder
+    public record SeatingResponse(Integer tableNum, LocalDateTime exitTime, UUID waitingId) {
+        public static SeatingResponse from(Seating seating) {
+            return SeatingResponse.builder()
+                    .tableNum(seating.getSeatingNum())
+                    .exitTime(seating.getWaiting().getExitTime())
+                    .waitingId(seating.getWaiting().getWaitingId())
+                    .build();
+        }
+    }
+}
