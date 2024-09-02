767c4008340592562ce9adf9404216878a88a386
노정완 <jeongwan.noh@gmail.com>
2024-09-02 16:41:54 +0900

    [GTB-68] feat: 웨이팅 입장 DTO 변경

--- src/main/java/site/gachontable/gachontablebe/domain/admin/presentation/dto/request/CallUserRequest.java
+++ src/main/java/site/gachontable/gachontablebe/domain/admin/presentation/dto/request/CallUserRequest.java
@@ -2,6 +2,5 @@ package site.gachontable.gachontablebe.domain.admin.presentation.dto.request;
 
 import java.util.UUID;
 
-public record CallUserRequest(
-        UUID waitingId) {
+public record CallUserRequest(UUID waitingId) {
 }
