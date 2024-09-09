d67d1eba11024cf623e30d83488232aa003780d4
노정완 <jeongwan.noh@gmail.com>
2024-09-09 23:54:38 +0900

    [GTB-73] feat: 퇴장 처리 로직 구현

---
+++ src/main/java/site/gachontable/gachontablebe/domain/admin/usecase/ExitUserImpl.java
@@ -0,0 +1,45 @@
+package site.gachontable.gachontablebe.domain.admin.usecase;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.gachontable.gachontablebe.domain.admin.domain.repository.AdminRepository;
+import site.gachontable.gachontablebe.domain.admin.exception.AdminNotFoundException;
+import site.gachontable.gachontablebe.domain.admin.presentation.dto.request.ExitUserRequest;
+import site.gachontable.gachontablebe.domain.auth.domain.AuthDetails;
+import site.gachontable.gachontablebe.domain.pub.domain.Pub;
+import site.gachontable.gachontablebe.domain.pub.exception.PubMismatchException;
+import site.gachontable.gachontablebe.domain.seating.domain.Seating;
+import site.gachontable.gachontablebe.domain.seating.domain.respository.SeatingRepository;
+import site.gachontable.gachontablebe.domain.seating.exception.SeatingNotFoundException;
+import site.gachontable.gachontablebe.global.success.SuccessCode;
+
+@Service
+@RequiredArgsConstructor
+public class ExitUserImpl implements ExitUser{
+    private final SeatingRepository seatingRepository;
+    private final AdminRepository adminRepository;
+
+    @Override
+    @Transactional
+    public String execute(AuthDetails authDetails, ExitUserRequest request) {
+        Seating seating = seatingRepository.findBySeatingId(request.seatingId())
+                .orElseThrow(SeatingNotFoundException::new);
+
+        checkPubMatches(authDetails, seating);
+
+        seating.updateExitTime();
+
+        return SuccessCode.EXIT_USER_SUCCESS.getMessage();
+    }
+
+    private void checkPubMatches(AuthDetails authDetails, Seating seating) {
+        Pub pub = adminRepository.findById(authDetails.getUuid())
+                .orElseThrow(AdminNotFoundException::new)
+                .getPub();
+
+        if (!seating.getPub().equals(pub)) {
+            throw new PubMismatchException();
+        }
+    }
+}
--- src/main/java/site/gachontable/gachontablebe/domain/seating/domain/respository/SeatingRepository.java
+++ src/main/java/site/gachontable/gachontablebe/domain/seating/domain/respository/SeatingRepository.java
@@ -6,9 +6,12 @@ import site.gachontable.gachontablebe.domain.seating.domain.Seating;
 import site.gachontable.gachontablebe.domain.user.domain.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SeatingRepository extends JpaRepository<Seating, Long> {
     List<Seating> findAllByPubOrderBySeatingNum(Pub pub);
 
     List<Seating> findAllByUser(User user);
+
+    Optional<Seating> findBySeatingId(Integer seatingId);
 }
