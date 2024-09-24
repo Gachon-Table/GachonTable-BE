e08da8742e8d0b42630ec8abcedc5814b27d2d47
노정완 <jeongwan.noh@gmail.com>
2024-09-24 20:12:40 +0900

    [GTB-76] fix: 오탈자 수정

--- src/main/java/site/gachontable/gachontablebe/domain/shared/Table.java
+++ src/main/java/site/gachontable/gachontablebe/domain/shared/Table.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Table {
     BASIC("4인 테이블 (1~5인)"),
-    PARTY("8인 테이블 (5~8인)"),;
+    PARTY("8인 테이블 (5~8인)");
 
     private final String nameKo;
 }
