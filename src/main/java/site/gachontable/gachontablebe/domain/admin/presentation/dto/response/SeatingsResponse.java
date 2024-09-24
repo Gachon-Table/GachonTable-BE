9f37385ae186c3aba495012f0419f0eee0be2170
노정완 <jeongwan.noh@gmail.com>
2024-09-24 22:59:21 +0900

    [GTB-76] fix: 오탈자 수정 tableNum -> seatingNum

--- src/main/java/site/gachontable/gachontablebe/domain/admin/presentation/dto/response/SeatingsResponse.java
+++ src/main/java/site/gachontable/gachontablebe/domain/admin/presentation/dto/response/SeatingsResponse.java
@@ -12,12 +12,12 @@ import static site.gachontable.gachontablebe.domain.shared.DateTimeFormatters.WI
 public record SeatingsResponse(List<SeatingResponse> seatings) {
 
     @Builder
-    public record SeatingResponse(Integer seatingId, Integer tableNum, Table tableType, String exitTime, UUID waitingId) {
+    public record SeatingResponse(Integer seatingId, Integer seatingNum, Table tableType, String exitTime, UUID waitingId) {
         public static SeatingResponse from(Seating seating) {
 
             return SeatingResponse.builder()
                     .seatingId(seating.getSeatingId())
-                    .tableNum(seating.getSeatingNum())
+                    .seatingNum(seating.getSeatingNum())
                     .tableType(seating.getTableType())
                     .exitTime(seating.getExitTime().format(WITH_WEEKDAY))
                     .waitingId(seating.getWaiting().getWaitingId())
