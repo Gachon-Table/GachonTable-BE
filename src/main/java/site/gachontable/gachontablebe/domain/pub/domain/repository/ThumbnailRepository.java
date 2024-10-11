3aadf1d56ae7d402533ee2baa2ab44972222f5c7
노정완 <jeongwan.noh@gmail.com>
2024-10-12 01:06:05 +0900

    [GTB-83] feat: Thumbnail 리포지토리 생성

---
+++ src/main/java/site/gachontable/gachontablebe/domain/pub/domain/repository/ThumbnailRepository.java
@@ -0,0 +1,14 @@
+package site.gachontable.gachontablebe.domain.pub.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import site.gachontable.gachontablebe.domain.pub.domain.Pub;
+import site.gachontable.gachontablebe.domain.pub.domain.Thumbnail;
+
+import java.util.List;
+
+public interface ThumbnailRepository extends JpaRepository<Thumbnail, Long> {
+
+    @Query("SELECT t.url FROM thumbnail t WHERE t.pub = :pub")
+    List<String> findAllByPub(Pub pub);
+}
