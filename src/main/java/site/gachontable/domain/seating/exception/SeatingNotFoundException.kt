599cdfbd3892d8a8b7e83deb27f178dbac4ed8e5
Jeongwan Noh <jwnnoh@gmail.com>
2025-02-10 17:24:55 +0900

    [LUPG-14] refactor: Seating 도메인 예외처리 및 JPA 계층 마이그레이션

--- src/main/java/site/gachontable/domain/seating/exception/SeatingNotFoundException.kt
+++ src/main/java/site/gachontable/domain/seating/exception/SeatingNotFoundException.kt
@@ -1,10 +1,6 @@
-package site.gachontable.domain.seating.exception;
+package site.gachontable.domain.seating.exception
 
-import site.gachontable.independent.type.ErrorCode;
-import site.gachontable.independent.error.exception.ServiceException;
+import site.gachontable.independent.error.exception.ServiceException
+import site.gachontable.independent.type.ErrorCode
 
-public class SeatingNotFoundException extends ServiceException {
-    public SeatingNotFoundException() {
-        super(ErrorCode.SEATING_NOT_FOUND);
-    }
-}
+class SeatingNotFoundException : ServiceException(ErrorCode.SEATING_NOT_FOUND)
--- src/main/java/site/gachontable/domain/seating/port/out/SeatingRepository.kt
+++ src/main/java/site/gachontable/domain/seating/port/out/SeatingRepository.kt
@@ -1,21 +1,18 @@
-package site.gachontable.domain.seating.port.out;
+package site.gachontable.domain.seating.port.out
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import site.gachontable.domain.pub.domain.Pub;
-import site.gachontable.domain.seating.domain.Seating;
-import site.gachontable.domain.waiting.domain.Waiting;
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import site.gachontable.domain.pub.domain.Pub
+import site.gachontable.domain.seating.domain.Seating
+import site.gachontable.domain.waiting.domain.Waiting
+import java.time.LocalDateTime
+import java.util.*
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
-public interface SeatingRepository extends JpaRepository<Seating, Long> {
-
+interface SeatingRepository : JpaRepository<Seating, Long> {
     @Query("SELECT s.exitTime FROM seating s WHERE s.waiting = :waiting")
-    Optional<LocalDateTime> findExitTimeByWaiting(Waiting waiting);
+    fun findExitTimeByWaiting(waiting: Waiting): Optional<LocalDateTime>
 
-    boolean existsByPubAndSeatingNumAndExitTimeAfter(Pub pub, Integer seatingNum, LocalDateTime now);
+    fun existsByPubAndSeatingNumAndExitTimeAfter(pub: Pub, seatingNum: Int, now: LocalDateTime): Boolean
 
-    List<Seating> findAllByPubAndExitTimeAfterOrderByExitTime(Pub pub, LocalDateTime now);
+    fun findAllByPubAndExitTimeAfterOrderByExitTime(pub: Pub, now: LocalDateTime): MutableList<Seating>
 }
